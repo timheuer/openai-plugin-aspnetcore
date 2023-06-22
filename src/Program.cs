d408096cf2ac860591837793d39b0ceba8128597
Tim Heuer <tim@timheuer.com>
2023-06-22 12:04:35 -0700

    Adding back in CORS support for localhost scenario

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,4 @@
 using System.Text.Json;
-using OpenAIPluginMiddleware;
 
 // get some fake data
 var products = JsonSerializer.Deserialize<List<Product>>(File.ReadAllText("./Data/products.json"));
@@ -10,6 +9,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "Contoso Product Search", Version = "v1", Description = "Search through Contoso's wide range of outdoor and recreational products." });
+});
+
+// For certain scenarios to work with OpenAI, CORS needs to be enabled per documentation here: https://beta.openai.com/docs/developer-quickstart/your-first-api-request
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("OpenAI", policy =>
+    {
+        policy.WithOrigins("https://chat.openai.com");
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+    });
 });
 
 builder.Services.AddAiPluginGen(options =>
@@ -26,8 +36,15 @@ builder.Services.AddAiPluginGen(options =>
 
 var app = builder.Build();
 
+app.UseCors("OpenAI");
+
+// Static files here are being used only to serve the logo.png
+// This can be served from anywhere (CDN, blob storage, etc.) and if so, this can be removed.
 app.UseStaticFiles();
+
+// This ensures the OpenAPI definition is served for the plugin
 app.UseSwagger();
+
 app.UseAiPluginGen();
 
 if (app.Environment.IsDevelopment())
